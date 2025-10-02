b0f9dcf0aa548e16160c8d374f31e8b14aa2d86a
djw8889-sys <djw8889@gmail.com>
2025-10-03 08:23:44 +0900

    fix: resolve path alias for vite and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Vite가 'client' 폴더를 기준으로 작동하도록 설정합니다.
   root: 'client',
   build: {
-    // 빌드 결과물이 프로젝트 루트의 'dist' 폴더에 생성되도록 경로를 수정합니다.
     outDir: '../dist',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client/src"),
+    },
   },
   server: {
     proxy: {
